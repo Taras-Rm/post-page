dde4bd8ae7e21130e87f45c8a136f9fe23644032
Taras-Rm <tarasrom25@gmail.com>
2021-08-25 13:58:30 +0300

    editing login validation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import MyInput from "../../components/UI/MyInput";
 import MyButton from "../../components/UI/MyButton";
 import s from "./Login.module.css";
@@ -7,18 +7,29 @@ import AuthContext from "../../context";
 const Login = () => {
 
     const {isAuth, setIsAuth} = useContext(AuthContext);
+    const [authData, setAuthData] = useState({login: '', password: ''});
 
+    const handleChangePass = e => {
+        let inpText = e.target.value;
+        setAuthData({...authData, password: inpText });
+    }
+    const handleChangeLogin = e => {
+        let inpText = e.target.value;
+        setAuthData({...authData, login: inpText });
+    }
     const login = (e) => {
         e.preventDefault();
-        setIsAuth(true)
+
+        !(authData.login.length && authData.password.length) ? alert('Enter login and password !!!') : setIsAuth(true);
+
         localStorage.setItem('auth', 'true');
     }
 
     return (
         <form className={s.login} onSubmit={login}>
             <h1>Авторизація</h1>
-            <MyInput className={s.login_login} type="text" placeholder="Login"/>
-            <MyInput className={s.login_pass} type="password" placeholder="Password"/>
+            <MyInput className={s.login_login} onChange={(e) => handleChangeLogin(e)} type="text" placeholder="Login" />
+            <MyInput className={s.login_pass} onChange={(e) => handleChangePass(e)} type="password" placeholder="Password" />
             <MyButton className={s.login_btn}>Увійти</MyButton>
         </form>
 
