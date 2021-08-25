ca17742288420c4b8f3a68cd62fb787c5586e23d
Taras-Rm <tarasrom25@gmail.com>
2021-08-25 13:33:45 +0300

    exit button setting

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, {useContext} from "react";
-import {NavLink} from "react-router-dom";
+import React, {useContext, useEffect} from "react";
+import {NavLink, useHistory} from "react-router-dom";
 import s from './Navbar.module.css';
 import MyButton from "../MyButton";
 import AuthContext from "../../../context";
@@ -13,13 +13,16 @@ const Navbar = () => {
         localStorage.removeItem('auth');
     }
 
+
     return (
         <div className={s.navbar}>
             <div className={s.navbar_links}>
-                <NavLink className={s.navbar_item} activeClassName={s.active_nav_item} to='/about'>About</NavLink>
                 <NavLink className={s.navbar_item} activeClassName={s.active_nav_item} to='/posts'>Posts</NavLink>
+                <NavLink className={s.navbar_item} activeClassName={s.active_nav_item} to='/about'>About</NavLink>
             </div>
-            <MyButton onClick={logout}>Вийти</MyButton>
+            {
+                isAuth && <MyButton onClick={logout}>Вийти</MyButton>
+            }
         </div>
     );
 }
